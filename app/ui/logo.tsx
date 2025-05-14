3b72fd36b35b47e99aff5bac9ec70aff925ba1dc
Paula Pniewska <pniewska.paula@gmail.com>
2025-05-14 20:11:01 +0200

    add Link components and ig icon

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,9 +1,10 @@
 import { playfairDisplay } from '@/app/ui/fonts';
+import Link from 'next/link'
 
 export default function Logo() {
     return (
-      <div className={`${playfairDisplay.className} text-3xl font-semibold text-gray-800 cursor-pointer hover:text-gray-400 transition-all duration-300`}>
-        Artist Name
+      <div className={`${playfairDisplay.className} text-3xl font-semibold text-gray-800 hover:text-gray-400 transition-all duration-300`}>
+        <Link href="/">Artist Name</Link>
       </div>
     );
   }
