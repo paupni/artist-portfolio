bd499598adf4ebddd73392db7940a424ada7f5f8
Paula Pniewska <pniewska.paula@gmail.com>
2025-05-14 16:41:22 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         Home content
+        <img src='https://ycep4hdyvmfuo63e.public.blob.vercel-storage.com/artworks/artwork-1-4GOJOMFJN8eqkJsv1HIaxjdUOj16sk.jpg' alt='' ></img>
       </main>
   );
 }
