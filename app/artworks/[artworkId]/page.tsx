d6ae52e03c02a38eced1f356cff99123e48bcc8e
Paula Pniewska <pniewska.paula@gmail.com>
2025-05-27 23:45:59 +0200

    add basic artwork page

--- app/artworks/[artworkId]/page.tsx
+++ app/artworks/[artworkId]/page.tsx
@@ -1,14 +1,25 @@
+// 'use client';
+// import React from "react";
+// import { useState } from "react";
 import { getArtwork } from "@/app/db/neon";
+import { playfairDisplay } from '@/app/ui/fonts';
 
 export default async function ArtworkDetails({ params } : { params: Promise<{artworkId: string}> }) {
 
     const artworkId = (await params).artworkId;
-    const artwork = await getArtwork(artworkId);    
+    const artwork = await getArtwork(artworkId);
+
 
     return (
-        <div>
-            <h1>Title of an Artwork {artworkId}</h1>
-            <p>Size: {artwork?.size}</p>
+        <div className="flex gap-16 max-h-90">
+            <div>
+                <img className="max-h-screen" src={artwork?.image_url}></img>
+            </div>
+            <div className="">
+                <h1 className={`${playfairDisplay.className} text-2xl font-semibold text-black`}>Title of an Artwork {artworkId}</h1>
+                <p className={`${playfairDisplay.className} text-lg py-2`}>Size: {artwork?.size}</p>
+                <p className="pt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, quos repellendus numquam blanditiis iure maxime et in natus, expedita esse voluptate ea libero incidunt! Eius exercitationem optio tempora corporis sint.</p>
+            </div>
         </div>
     )
 }
